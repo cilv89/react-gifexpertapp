86199cdb31bcebc928d3c47120838eb5a13257cb
César León <cesarivanleon@gmail.com>
2022-02-16 23:45:04 -0500

    Initial commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,13 @@
+export const getGifs = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=HVK5xNHZ9yLiVPjaO9One9ckC3FrL8mv`;
+  const respuesta = await fetch(url);
+  const { data } = await respuesta.json();
+  const gifs = data.map(img => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images?.downsized_medium.url,
+    };
+  });
+  return gifs;
+};
